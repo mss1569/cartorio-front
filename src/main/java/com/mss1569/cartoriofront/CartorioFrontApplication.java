5f4971441b0a8de2cbda8402d56065e1bc873778
mss1569 <msds1569@gmail.com>
2021-01-17 09:12:13 -0300

    spring start

---
+++ src/main/java/com/mss1569/cartoriofront/CartorioFrontApplication.java
@@ -0,0 +1,13 @@
+package com.mss1569.cartoriofront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartorioFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartorioFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mss1569/cartoriofront/CartorioFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mss1569.cartoriofront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartorioFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
